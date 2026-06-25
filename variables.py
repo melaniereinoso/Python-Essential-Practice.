faa02bc1129830a758d75daec56d96c9188bb894
Melanie Reinoso <melaniereinoso3103@gmail.com>
2026-06-25 15:42:17 -0500

    variable usage

---
+++ variables.py
@@ -0,0 +1,45 @@
+print("Hola mundo")
+
+hola = 1
+print(hola)
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+
+var = "3.8.5"
+print("Python version: " + var)
+ 
+ #reasignacion
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+
+#Resolución de problemas matemáticos sencillos
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+
+#create the variables: john, mary, and adam;
+#assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
+#having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+#now create a new variable named total_apples equal to the addition of the three previous variables.
+#print the value stored in total_apples to the console;
+
+john = 3
+mary = 5
+adam = 6
+print (john, mary, adam, sep= ",")
+total_apples = john+ mary + adam
+print(total_apples)
+
+
