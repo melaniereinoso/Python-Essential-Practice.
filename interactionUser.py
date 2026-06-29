574f68651864eb56a72f539512daa3ad890de8e4
Melanie Reinoso <melaniereinoso3103@gmail.com>
2026-06-29 09:34:18 -0500

    interaction with the user

---
+++ interactionUser.py
@@ -0,0 +1,22 @@
+#We're now going to introduce you to a completely new function, which seems to be a mirror reflection of the good old print() function.
+
+#Why? Well, print() sends data to the console.
+
+#The new function gets data from it.
+
+#print() has no usable result. The meaning of the new function is to return a very usable result.
+
+#The function is named input(). The name of the function says everything.
+
+#The input() function is able to read data entered by the user and to return the same data to the running program.
+
+#The program can manipulate the data, making the code truly interactive.
+
+#Virtually all programs read and process data. A program which doesn't get a user's input is a deaf program.
+
+#Take a look at our example:
+
+print("Tell me anything...")
+anything = input()
+print("Hmm...", anything, "... Really?")
+
